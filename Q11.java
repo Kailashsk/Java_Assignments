46bc70c1bb31ddf6941c092efa14551e8f6790f1
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:27:32 +0530

    Create Q11.java

---
+++ Q11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        double a = 2147483647;
+        System.out.println(a);
+        System.out.println(a+1);
+        System.out.println(2-a);
+        System.out.println(-2-a);
+
+    } 
+
+}
