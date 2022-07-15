0fae37113db65db5d2b506846e4f36506ebaf00e
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:24:52 +0530

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        int v1=113;
+        String v2="2.71828";
+        System.out.println("This is room # "+v1);
+        System.out.println("e is close to "+v2);
+
+    } 
+
+}
