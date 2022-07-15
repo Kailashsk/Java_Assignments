8b2bab00b5b12afc48a3504f82b81562a41e5d4b
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:28:27 +0530

    Create Q13.java

---
+++ Q13.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        String ruler1="1";
+        System.out.println(ruler1);
+        ruler1=ruler1+" 2 "+ruler1;
+        System.out.println(ruler1);
+        ruler1=ruler1+" 3 "+ruler1;
+        System.out.println(ruler1);
+        ruler1=ruler1+" 4 "+ruler1;
+        System.out.println(ruler1);
+
+    } 
+
+}
