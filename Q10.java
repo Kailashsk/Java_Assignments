c6fcaa88535f63fdc09e3ea7871a163cb535236f
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:27:04 +0530

    Create Q10.java

---
+++ Q10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        System.out.println('b');
+        System.out.println('b' + 'c');
+        System.out.println((char) ('a' + 4));
+    } 
+
+}
