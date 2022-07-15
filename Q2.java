6d492a367241318cd0b07c92007d8ec375ddc6be
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:21:59 +0530

    Create Q2.java

---
+++ Q2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        System.out.print("                                       ");
+        System.out.println("####" );
+        System.out.print("                                       ");
+        System.out.println("####" );
+        System.out.print("                                       ");
+        System.out.println("####" );
+        System.out.print("             ");
+        System.out.println("Kailash Chandra Sahu "+"\n             Bhubaneswar");
+    } 
+}
