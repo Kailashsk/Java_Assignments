dff749bcd21fe6676e73c86114276b5218bc5f01
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:24:29 +0530

    Create Q5.java

---
+++ Q5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        String regd="2161020058";
+        int year=2021;
+        System.out.println("My Regd. No is"+ regd + " and I have taken admission in MCA In " + year);
+    } 
+
+}
