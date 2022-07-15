2807bb3ac360ef8917ecf3c3fc270afc6641c3ae
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:22:34 +0530

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        System.out.println(" TTTTTTTT      K     K");
+        System.out.println("    T          K   K");
+        System.out.println("    T          K K");
+        System.out.println("    T          K");
+        System.out.println("    T          K K");
+        System.out.println("    T          K   K");
+        System.out.println("    T          K     K");
+
+    } 
+
+}
