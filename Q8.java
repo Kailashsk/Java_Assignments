450daa7fceecfa22f55002797c352a9009623f8f
Kailash Sahu <85280661+Kailashsk@users.noreply.github.com>
2022-07-16 04:25:49 +0530

    Create Q8.java

---
+++ Q8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class dis{
+
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of A & B");
+        
+        a=sc.nextInt();
+        b=sc.nextInt();
+        System.out.println("The Value of A & B is "+ b +" "+ a);
+    } 
+
+}
